135fa7fd3b1b56aab18fc858ec74701d2f2e2e9f
roy <roy.rosner@gmail.com>
2026-09-10 16:00:35 +0300

    .\NavBar.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,19 @@ import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import {BrowserRouter} from "react-router"
+import {BrowserRouter,Route,Routes} from "react-router"
+import NavBar from './NavBar.tsx'
+import Employees from './Employees.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-    <App />
+
+    <NavBar />
+   <Routes>
+    <Route path="/" element= {<App />}></Route>
+    <Route path="/employees" element= {<Employees />}></Route>
+   </Routes>
     </BrowserRouter>
   </StrictMode>,
 )
